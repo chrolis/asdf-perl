f7dac241fc158da92a39c9f1cf7e95b63c2cfe45
chrolis <chrolis@hatena.ne.jp>
2020-08-06 08:39:04 +0900

    feat: init

---
+++ lib/utils.sh
@@ -0,0 +1,62 @@
+PERL_BUILD_VERSION="${ASDF_PERL_BUILD_VERSION:-1.31}"
+PERL_BUILD_TAG="${PERL_BUILD_VERSION}"
+
+echoerr() {
+    >&2 echo -e "\033[0;31m$1\033[0m"
+}
+
+ensure_perl_build_setup() {
+    ensure_perl_build_installed
+}
+
+ensure_perl_build_installed() {
+    local perl_build_version
+
+    if [ ! -f "$(perl_build_path)" ]; then
+        download_perl_build
+    else
+        current_perl_build_version="$("$(perl_build_path)" --version | cut -d ' ' -f2)"
+        if [ "$current_perl_build_version" != "$PERL_BUILD_VERSION" ]; then
+            # If the perl-build directory already exists and the version does not
+            # match, remove it and download the correct version
+            rm -rf "$(perl_build_dir)"
+            download_perl_build
+        fi
+    fi
+}
+
+download_perl_build() {
+    # Print to stderr so asdf doesn't assume this string is a list of versions
+    echoerr "Downloading perl-build..."
+    local build_dir="$(perl_build_source_dir)"
+
+    # Remove directory in case it still exists from last download
+    rm -rf $build_dir
+
+    # Clone down and checkout the correct perl-build version
+    git clone https://github.com/tokuhirom/Perl-Build.git $build_dir >&2 >/dev/null
+    (cd $build_dir; git checkout $PERL_BUILD_TAG >&2 >/dev/null)
+
+    # Install in the perl-build dir
+    PREFIX="$(perl_build_dir)" $build_dir/install.sh
+
+    # Remove perl-build source dir
+    rm -rf $build_dir
+}
+
+
+asdf_perl_plugin_path() {
+    echo "$(dirname "$(dirname "$0")")"
+}
+
+perl_build_dir() {
+    echo "$(asdf_perl_plugin_path)/perl-build"
+}
+
+perl_build_source_dir() {
+    echo "$(asdf_perl_plugin_path)/perl-build-source"
+}
+
+perl_build_path() {
+    echo "$(perl_build_dir)/bin/perl-build"
+}
