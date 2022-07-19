2be88838e4ad912bc389fb30d0094f9b48125d01
chrolis <chrolis@hatena.ne.jp>
2022-07-19 23:07:13 +0900

    feat: use perl-install instead of perl-build

--- lib/utils.sh
+++ lib/utils.sh
@@ -1,60 +1,57 @@
-PERL_BUILD_VERSION="${ASDF_PERL_BUILD_VERSION:-1.32}"
-
 echoerr() {
     >&2 echo -e "\033[0;31m$1\033[0m"
 }
 
-ensure_perl_build_setup() {
-    ensure_perl_build_installed
+ensure_perl_install_setup() {
+    ensure_perl_install_installed
 }
 
-ensure_perl_build_installed() {
-    local perl_build_version
-
-    if [ ! -f "$(perl_build_path)" ]; then
-        download_perl_build
-    else
-        current_perl_build_version="$("$(perl_build_path)" --version | cut -d ' ' -f2)"
-        if [ "$current_perl_build_version" != "$PERL_BUILD_VERSION" ]; then
-            update_perl_build
-        fi
+ensure_perl_install_installed() {
+    if [ ! -f "$(perl_install_bin)" ]; then
+        download_perl_install
+    elif is_perl_install_invalidated; then
+        update_perl_install
     fi
 }
 
-download_perl_build() {
+download_perl_install() {
     # Print to stderr so asdf doesn't assume this string is a list of versions
-    echoerr "Downloading perl-build..."
-    local build_dir="$(perl_build_dir)"
+    echoerr "Downloading perl-install..."
 
-    # Clone down and checkout the correct perl-build version
-    git clone https://github.com/tokuhirom/Perl-Build.git $build_dir >&2 >/dev/null
-    checkout_perl_build
+    git clone --depth 1 https://github.com/skaji/perl-install "$(perl_install_dir)" >&2 >/dev/null
 }
 
-update_perl_build() {
-    echoerr "Updating perl-build..."
-    local build_dir="$(perl_build_dir)"
-    (cd $build_dir; git checkout master; git pull)
-    checkout_perl_build
+update_perl_install() {
+    echoerr "Updating perl-install..."
+
+    (cd "$(perl_install_dir)" && git fetch  && git reset --hard origin/HEAD)
 }
 
-checkout_perl_build() {
-    local build_dir="$(perl_build_dir)"
-    (cd $build_dir; git checkout $(git rev-list --topo-order ${PERL_BUILD_VERSION}..HEAD | tail -1) >&2 >/dev/null)
-}
+is_perl_install_invalidated() {
+    local validate_timeout=604800  # one week
+    local validate_file
+    validate_file="$(perl_install_dir)/.git/FETCH_HEAD"
 
-switch_perl_build() {
-    local build_dir="$(perl_build_dir)"
+    if [ ! -f "$validate_file" ]; then return; fi  # true
+
+    local last_update
+    local current_timestamp
+    last_update=$(date -r "$validate_file" +%s)
+    current_timestamp=$(date +%s)
+
+    local invalidated_at=$((last_update + validate_timeout))
+
+    [ "$invalidated_at" -lt "$current_timestamp" ]
 }
 
 asdf_perl_plugin_path() {
     echo "$(dirname "$(dirname "$0")")"
 }
 
-perl_build_dir() {
-    echo "$(asdf_perl_plugin_path)/perl-build"
+perl_install_dir() {
+    echo "$(asdf_perl_plugin_path)/perl-install"
 }
 
-perl_build_path() {
-    echo "$(perl_build_dir)/perl-build"
+perl_install_bin() {
+    echo "$(perl_install_dir)/perl-install"
 }
